a6eb7e745f3d88d97034a670c2f77670883fe452
nehasri001 <77269733+nehasri001@users.noreply.github.com>
2021-04-05 15:55:31 +0530

    Create array_sum.c

---
+++ array_sum.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+// how do we create an array
+
+// programmer maa paali* devatha
+
+// you are our goddess hail!!!! goddess Nehasri!!!!!!!!!!!!
+
+// okay now go to class
+
+// alright go now
+
+int main()
+{
+    int array_name[3] = {1,2,3};
+
+    int n;
+
+    for(int i = 0;i < 3; i ++)
+    {
+    
+    // ?????????????? shall we take "{}" or not????????????????????????????????????????????????
+
+    // hello!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        // next??????????????????????
+    
+        // that's it!
+        
+        // and "{}" yes you donkey
+
+        n += array_name [i]; // great!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! but if it does not work
+
+        // i will be the most angriet person
+
+        // do note that down
+
+    }
+        printf("%d",n);
+
+        return 0;
+    // great!!!!!!!!!!!!!!!!
+}
+
+
+// great!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// you donkey*2, idiot shinchan
+
+// great!!!!!!!!!!!
+
+// congratulations!!!!!!!!!!!!!!
+
+// lol
+
+// you  are great
+
+// wonderful
+
+// i am extremist
