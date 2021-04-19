1d82dfa8ea513b3b3144a5a5f34c8ff2d3788b8d
nehasri001 <77269733+nehasri001@users.noreply.github.com>
2021-04-19 09:48:33 +0530

    Add files via upload

---
+++ User_defined_exceptios.cs
@@ -0,0 +1,46 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_defined_Exceptions
+{
+    public class : Exception
+    {
+        public Out_of_range()
+        {
+            Console.WriteLine("Can't go beyond the limits");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[5] { 1, 2, 3, 4, 5 };
+
+
+            try
+            {
+                for(int i = 0; i < 6;i ++)
+                {
+                    if(i > 4)
+                    {
+                        throw new Out_of_range();
+                    }
+                    else
+                    {
+                        Console.WriteLine(array[i]);
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Went out of limit");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
